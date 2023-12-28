a4193729d0c405684a2fa2bb35713190fc6582f7
Liam Congleton <greenonions117@gmail.com>
2023-12-28 15:40:01 -0500

    "Update package.json version"

--- src/utils/pdfUtils.js
+++ src/utils/pdfUtils.js
@@ -1,6 +1,14 @@
 import { PDFDocument, PDFPage } from 'pdf-lib';
+import pdf from 'pdf-parse';
 import fs from 'fs';
 
+//allows for the pdf to be passed in as an argument from the command line
+let pdfPath = process.argv[2];
+getPDFDimensions(pdfPath);
+extractTextFromPdf(pdfPath, '../../test_files');
+
+
+//need to account for edge cases such as different sized pages in the pdf
 async function getPDFDimensions(pdfPath) {
   const pdfBytes = fs.readFileSync(pdfPath);
   const pdfDoc = await PDFDocument.load(pdfBytes);
@@ -12,5 +20,14 @@ async function getPDFDimensions(pdfPath) {
   console.log(`Width: ${width}, Height: ${height}`);
 }
 
-const pdfPath = process.argv[2];
-getPDFDimensions(pdfPath);
+ 
+//This function will take in a pdf file and return the text of pages in the pdf
+ function extractTextFromPdf(pdfFilePath, outputDir) {
+    let dataBuffer = fs.readFileSync(pdfFilePath);
+
+    pdf(dataBuffer).then(function(data) {
+        let outputFilePath = pdfPath.join(outputDir, pdfPath.basename(pdfFilePath, '.pdf') + '.txt');
+        fs.writeFileSync(outputFilePath, data.text);
+    });
+}
+
