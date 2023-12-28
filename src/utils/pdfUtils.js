4aafc56a3625afd0801c43410f5ddd4e669cfd8d
Liam Congleton <greenonions117@gmail.com>
2023-12-28 16:49:24 -0500

    added function to test for encryption

--- src/utils/pdfUtils.js
+++ src/utils/pdfUtils.js
@@ -5,9 +5,11 @@ import path from 'path'; // import the path module
 
 //allows for the pdf to be passed in as an argument from the command line
 let pdfPath = process.argv[2];
+
+//call the functions
 getPDFDimensions(pdfPath);
 extractTextFromPdf(pdfPath, '../../test_output');
-
+isPdfEncrypted(pdfPath);
 
 //need to account for edge cases such as different sized pages in the pdf
 async function getPDFDimensions(pdfPath) {
@@ -23,11 +25,18 @@ async function getPDFDimensions(pdfPath) {
 
 
 //This function will take in a pdf file and return the text of pages in the pdf
- function extractTextFromPdf(pdfFilePath, outputDir) {
+async function extractTextFromPdf(pdfFilePath, outputDir) {
     let dataBuffer = fs.readFileSync(pdfFilePath);
 
     pdf(dataBuffer).then(function(data) {
         let outputFilePath = path.join(outputDir, path.basename(pdfFilePath, '.pdf') + '.txt'); // use path.basename
         fs.writeFileSync(outputFilePath, data.text);
     });
+}
+
+async function isPdfEncrypted(pdfPath) {
+  const pdfBytes = fs.readFileSync(pdfPath);
+  const pdfDoc = await PDFDocument.load(pdfBytes, { ignoreEncryption: true });
+  console.log('PDF Encrypted: ' + pdfDoc.isEncrypted);
+  return pdfDoc.isEncrypted;
 }
